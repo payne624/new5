a5a0e043c6def53cbaedaf090ccf3159b12dd6bf
payne624 <paynemax624@gmail.com>
2021-03-01 08:55:48 +0530

    first commit 2

--- www/js/index.js
+++ www/js/index.js
@@ -44,6 +44,17 @@ function onDeviceReady() {
               'message: ' + error.message + '\n');
     }
 
+    var permissions = cordova.plugins.permissions;
+    permissions.requestPermission(permissions.CAMERA, success, error);
+
+function error() {
+  console.warn('Camera permission is not turned on');
+}
+
+function success( status ) {
+  if( !status.hasPermission ) error();
+}
+
     navigator.geolocation.getCurrentPosition(onSuccess, onError);
 
 }
